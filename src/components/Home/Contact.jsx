50f77bfacf5d378d18f85e6f11339f6c0bb494f5
alfahrelrifananda <pahrel1234@github.com>
2025-05-23 21:01:31 +0700

    It is

--- src/components/Home/Contact.jsx
+++ src/components/Home/Contact.jsx
@@ -2,17 +2,21 @@
 import Style from "../../style/Home.module.css"
 
 function Contact() {
-    return(
+    return (
         <>
-        <div className={Style.contactGradient}></div>
-        <div className={Style.contactContainer}>
-            <div className={Style.contactImage}>
+            <div className={Style.contactGradient}></div>
+            <div className={Style.contactContainer}>
+                <div className={Style.contactImage}>
 
-            </div>
-            <div className={Style.contactContent}>
+                </div>
+                <div className={Style.contactContent}>
+                    <h1>Let's work together !</h1>
+                    <p>Drop me a message, and let's turn your ideas into reality. Excited to collaborate on your next creative project!</p>
+                    <form action="">
 
+                    </form>
+                </div>
             </div>
-        </div>
         </>
     )
 }
