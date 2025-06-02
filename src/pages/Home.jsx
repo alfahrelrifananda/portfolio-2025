a120cc17877fe40cabae8dfeb56e447bc8ace70e
alfahrelrifananda <pahrel1234@github.com>
2025-06-02 12:18:46 +0700

    Ya

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,7 +16,7 @@ function Home() {
             <About />
             <AboutMore />
             <div id="blogTreshold"></div>
-            <MyBlog />
+            {/* <MyBlog /> */}
             <Contact />
 
         </>
