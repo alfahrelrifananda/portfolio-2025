db9d2471e5f3d9c9c1d2a294e2024a2c8a1ca3d7
alfahrelrifananda <pahrel1234@github.com>
2025-06-08 14:15:37 +0700

    fdsf

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { compression } from 'vite-plugin-compression2';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    compression()
+  ],
   base: "/portfolio-2025",
   build: {
     chunkSizeWarningLimit: 1600,
   },
 })
+
+
